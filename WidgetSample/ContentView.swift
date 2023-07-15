f04083ebc91c2bce4e06d2226319de6088af84e1
ji-no <jino.jpn@gmail.com>
2023-07-15 22:09:47 +0900

    Initial Project

---
+++ WidgetSample/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WidgetSample
+//  
+//  Created by ji-no on 2023/07/15
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetSample/WidgetSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetSampleApp.swift
+//  WidgetSample
+//  
+//  Created by ji-no on 2023/07/15
+//  
+//
+
+import SwiftUI
+
+@main
+struct WidgetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
