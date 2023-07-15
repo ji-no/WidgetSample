bc6ff0678f025aa7f3504a8636a79442086cdd7b
ji-no <jino.jpn@gmail.com>
2023-07-15 23:22:02 +0900

    add Widget Target

---
+++ SimpleWidget/SimpleWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWidgetBundle.swift
+//  SimpleWidgetExtension
+//  
+//  Created by ji-no on 2023/07/15
+//  
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SimpleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SimpleWidget()
+    }
+}
